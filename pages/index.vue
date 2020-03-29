1ed944fd26dd79d5d5e3f28a1269ebcfc3f8be46
35d <xxx35d@gmail.com>
2020-03-29 21:17:26 +0900

    ✨ slack test

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        fast-notion-lp
-      </h1>
-      <h2 class="subtitle">
-        My exceptional Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      Fast Notion
     </div>
   </div>
 </template>
