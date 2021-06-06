f8c69bbb4fdf10e9f3e852574c733345e5896487
35d <xxx35d@gmail.com>
2021-06-07 00:22:36 +0900

    ✨ [WIP] create logic

---
+++ config.ts
@@ -0,0 +1,2 @@
+export const NOTION_VERSION = '2021-05-13'
+export const BASE_NOTION_API_URL = 'https://api.notion.com/v1'
--- pages/playground/index.vue
+++ pages/playground/index.vue
@@ -1,41 +1,113 @@
 <template>
   <div>
+    <h1>Fast Notion Web (β)</h1>
     <div class="todo">
       <p>TODO リスト</p>
       <p>- [x] Composition API 導入</p>
       <p>
-        - [ ] TODO トークンを入力してローカルストレージに保存できるようにする
+        - [x] TODO トークン・URL を入力できるようにする
       </p>
-      <p>- [ ] TODO URL を入力してローカルストレージに保存できるようにする</p>
+
       <p>- [ ] API 接続して、投稿できるようにする</p>
-      <p>{{ state }}</p>
+      <p>- [ ] ストレージに保存できるようにする</p>
+      <hr />
     </div>
+    <form>
+      <label>
+        <span>
+          TOKEN
+        </span>
+        <input
+          v-model="state.settingForm.token"
+          type="text"
+          placeholder="INPUT YOUR TOKEN"
+        />
+      </label>
+      <label>
+        <span>
+          URL
+        </span>
+        <input
+          v-model="state.settingForm.url"
+          type="text"
+          placeholder="INPUT YOUR TOKEN"
+        />
+      </label>
+    </form>
+    <form>
+      <label>
+        <span>
+          TEXT
+        </span>
+        <input
+          v-model="state.postForm.text"
+          type="text"
+          placeholder="INPUT YOUR NOTE"
+        />
+      </label>
+      <button @click.prevent="_onClickSubmitButton">POST</button>
+    </form>
+    {{ state }}
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from '@nuxtjs/composition-api'
 
+const baseUrl = 'https://fast-notion.appspot.com/v1'
+export const addUrl = `${baseUrl}/add`
+export const addDbUrl = `${baseUrl}/dbAdd`
+
 interface SettingForm {
   url: string
   token: string
 }
 
+interface PostForm {
+  text: string
+}
+
 interface State {
   settingForm: SettingForm
+  postForm: PostForm
+}
+
+export interface PostBody {
+  key: string
+  // eslint-disable-next-line
+  token_v2: string
+  // eslint-disable-next-line
+  notion_url: string
+  text: string
+  type: string
 }
 
 export default defineComponent({
-  setup() {
-    // // TODO hooks に切り出す
+  setup(_props, { root }) {
     const state = reactive<State>({
       settingForm: {
         url: '',
         token: ''
+      },
+      postForm: {
+        text: ''
       }
     })
 
-    return { state }
+    const _onClickSubmitButton = () => {
+      const data: PostBody = {
+        key: '5NGb6uKrbSW4Q6SMsJL8sYuBY4ZFPYD',
+        token_v2: state.settingForm.token,
+        notion_url: state.settingForm.url,
+        text: state.postForm.text,
+        type: ''
+      }
+      root.$axios.post(addUrl, data)
+
+      alert('@')
+    }
+
+    return { state, _onClickSubmitButton }
   }
 })
 </script>
