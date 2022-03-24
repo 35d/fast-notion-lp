aced1edc0ae9d315d3f8ef8267762e0b77d1619b
35d <xxx35d@gmail.com>
2022-03-24 22:57:36 +0900

    🐛 fix img path

--- components/container/top/FunctionSection.vue
+++ components/container/top/FunctionSection.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
-    <div
-      class="flex flex-col mt-24 mb-14 md:flex-row md:w-4/5 md:mx-auto md:max-w-4xl md:items-center md:justify-center"
-    >
+    <div class="flex flex-col mt-24 mb-14 md:flex-row md:w-4/5 md:mx-auto md:max-w-4xl md:items-center md:justify-center">
       <div class="mx-9 mb-14 md:w-1/2">
-        <h2
-          class="font-bold text-2xl text-center text-black1 mb-4 md:text-left"
-        >
-          Fast Notionでできること
-        </h2>
+        <h2 class="font-bold text-2xl text-center text-black1 mb-4 md:text-left">Fast Notionでできること</h2>
         <p
           class="text-xs font-bold tracking-wider text-center mb-8 text-transparent bg-clip-text bg-grad-text md:text-left md:text-sm"
         >
@@ -23,9 +17,7 @@
         <img src="~@/assets/img/top/function.png" alt="" class="w-11/12" />
       </div>
     </div>
-    <h3 class="text-center mt-24 mb-10 font-medium text-xl">
-      自由にメモ形式を指定できる
-    </h3>
+    <h3 class="text-center mt-24 mb-10 font-medium text-xl">自由にメモ形式を指定できる</h3>
     <div class="mb-8 md:flex md:flex-wrap md:w-4/5 md:mx-auto md:max-w-4xl">
       <template v-for="cando in candos" :key="index">
         <div class="flex flex-col px-6 pb-8 md:w-1/2 md:px-4">
@@ -42,44 +34,29 @@
       </template>
     </div>
     <div class="bg-gray1 py-16 px-6 flex flex-col items-center">
-      <h3 class="text-black1 text-xl font-medium mb-4">
-        多くの方に評価されています
-      </h3>
-      <p class="text-gray2 text-xs font-medium mb-10">
-        ※2022年2月時点、650件の評価。
-      </p>
+      <h3 class="text-black1 text-xl font-medium mb-4">多くの方に評価されています</h3>
+      <p class="text-gray2 text-xs font-medium mb-10">※2022年2月時点、650件の評価。</p>
       <div class="flex gap-4 mb-10 w-full md:w-4/5 md:max-w-4xl">
-        <div
-          class="bg-white py-10 px-6 w-1/2 shadow-3xl rounded flex flex-col justify-center"
-        >
+        <div class="bg-white py-10 px-6 w-1/2 shadow-3xl rounded flex flex-col justify-center">
           <p
             class="font-[Futura] text-[64px] font-medium text-transparent bg-clip-text bg-grad-text text-center relative after:absolute after:top-4 after:content-['※'] after:text-xs after:text-black1 after:font-sans after:-right-1 after:md:right-1/4 after:lg:right-1/3"
           >
             4.6
           </p>
-          <p class="text-black1 text-sm font-bold text-center md:text-base">
-            App Store評価
-          </p>
+          <p class="text-black1 text-sm font-bold text-center md:text-base">App Store評価</p>
         </div>
-        <div
-          class="bg-white py-10 px-4 w-1/2 shadow-3xl rounded flex flex-col justify-center"
-        >
-          <p
-            class="font-[Futura] text-4xl font-medium text-transparent bg-clip-text bg-grad-text text-center mb-4 md:text-6xl"
-          >
+        <div class="bg-white py-10 px-4 w-1/2 shadow-3xl rounded flex flex-col justify-center">
+          <p class="font-[Futura] text-4xl font-medium text-transparent bg-clip-text bg-grad-text text-center mb-4 md:text-6xl">
             30,000
           </p>
-          <p class="text-black1 text-sm font-bold text-center md:text-base">
-            累計ダウンロード数
-          </p>
+          <p class="text-black1 text-sm font-bold text-center md:text-base">累計ダウンロード数</p>
         </div>
       </div>
       <div class="w-full h-16 flex justify-center items-center">
         <a href="https://apps.apple.com/jp/app/fast-notion/id1505194382"
           ><img src="~@/assets/img/parts/download-ios.svg" alt=""
         /></a>
-        <a
-          href="https://play.google.com/store/apps/details?id=jp.fastNotion&hl=ja&gl=US"
+        <a href="https://play.google.com/store/apps/details?id=jp.fastNotion&hl=ja&gl=US"
           ><img src="~@/assets/img/parts/download-android.svg" alt=""
         /></a>
       </div>
@@ -92,22 +69,22 @@ const candos = [
   {
     title: "データベース接続",
     content: "データベースに直接テキストメモを送信",
-    imgPath: "/static/img/function-database.png",
+    imgPath: "/public/img/function-database.png",
   },
   {
     title: "複数ページの指定",
     content: "Notion上の好きなページを指定して、メモを投稿",
-    imgPath: "static/img/function-multi.png",
+    imgPath: "public/img/function-multi.png",
   },
   {
     title: "To Do モード対応",
     content: "思いついたタスクを即時に追加",
-    imgPath: "/img/function-todo.png",
+    imgPath: "/public/img/function-todo.png",
   },
   {
     title: "ダークモード対応",
     content: "視覚的に読みやすい画面に変更可能",
-    imgPath: "img/function-dark.png",
+    imgPath: "/public/img/function-dark.png",
   },
 ];
 </script>
