c2f11e6452a4b9ea1a9a5cc74f24ecd8e054fdf3
35d <xxx35d@gmail.com>
2022-04-12 20:02:42 +0900

    ✨ add og-image

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <NuxtLayout>
+    <Head>
+      <Meta property="og:title" content="Fast Notion" />
+      <Meta property="og:url" content="https://fast-notion.com/" />
+      <Meta property="og:image" content="/img/og-image.jpg" />
+      <Meta property="og:site_name" content="Fast Notion" />
+      <Meta property="og:description" content="ページのディスクリプション" />
+      <Meta name="twitter:card" content="summary_large_image" />
+    </Head>
     <NuxtPage />
   </NuxtLayout>
 </template>
