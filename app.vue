8b5e9bd0e41b1c8424abf594b26bd92cc9b5abd6
35d <xxx35d@gmail.com>
2022-04-12 20:06:22 +0900

    ♻️ fix meta tags

--- app.vue
+++ app.vue
@@ -3,9 +3,12 @@
     <Head>
       <Meta property="og:title" content="Fast Notion" />
       <Meta property="og:url" content="https://fast-notion.com/" />
-      <Meta property="og:image" content="/img/og-image.jpg" />
+      <Meta property="og:image" content="https://fast-notion.com/img/og-image.jpg" />
       <Meta property="og:site_name" content="Fast Notion" />
-      <Meta property="og:description" content="ページのディスクリプション" />
+      <Meta
+        property="og:description"
+        content="Fast Notion はスピーディーに起動し、素早く Notion にメモを取ることができるスマートフォンアプリです"
+      />
       <Meta name="twitter:card" content="summary_large_image" />
     </Head>
     <NuxtPage />
