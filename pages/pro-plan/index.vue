a36d27418dbe171eca064d66b99bed2542a5a94f
35d <xxx35d@gmail.com>
2022-11-19 23:01:42 +0900

    ✨ add pro-plan page

---
+++ pages/pro-plan/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+
+    // Fast Notion アプリ内のリンクが /pro-plan となっており、
+    // このページに来ると 404 になってしまうため
+    router.replace("/faq");
+  },
+});
+</script>
