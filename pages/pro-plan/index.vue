8d817f6e4a601cf90f78110956f66e3dc239c23c
35d <xxx35d@gmail.com>
2022-11-19 23:31:20 +0900

    ♻️ test

--- pages/pro-plan/index.vue
+++ pages/pro-plan/index.vue
@@ -3,11 +3,11 @@
 </template>
 
 <script setup lang="ts">
-const router = useRouter();
+// const router = useRouter();
 
 // Fast Notion アプリ内のリンクが /pro-plan となっており、
 // このページに来ると 404 になってしまうため
 // router.push("/faq");
 console.log("@");
-router.replace("/faq");
+// router.replace("/faq");
 </script>
