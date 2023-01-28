9ad122ef79c1ec6aed9c9d9db5d359d29538e680
35d <xxx35d@gmail.com>
2023-01-28 22:38:40 +0900

    ✨ setup i18n and add sample code

--- components/presentational/Header.vue
+++ components/presentational/Header.vue
@@ -2,50 +2,27 @@
   <header class="z-10 flex justify-center">
     <div class="h-14 md:h-16 mt-2 md:mt-4 flex justify-between items-center w-[calc(100vw-48px)] md:w-4/5 md:max-w-7xl">
       <nuxt-link to="/">
-        <img src="~@/assets/img/parts/fast-notion-white.svg" alt="" v-if="shouldHeaderBgTransparentMode" /><img
-          src="~@/assets/img/parts/fast-notion-black.svg"
-          alt=""
-          class="w-[110px]"
-          v-else
+        <img src="~@/assets/img/parts/fast-notion-white.svg" alt="" v-if="shouldHeaderBgTransparentMode" /><img src="~@/assets/img/parts/fast-notion-black.svg" alt="" class="w-[110px]" v-else
       /></nuxt-link>
       <div class="hidden md:flex md:gap-8">
         <template v-for="content in headerList" :key="content.title">
-          <nuxt-link
-            :to="content.path"
-            :class="[
-              'text-sm font-bold ',
-              { 'text-white': shouldHeaderBgTransparentMode },
-              { 'text-black1': !shouldHeaderBgTransparentMode },
-            ]"
-            >{{ content.title }}</nuxt-link
-          >
+          <nuxt-link :to="content.path" :class="['text-sm font-bold ', { 'text-white': shouldHeaderBgTransparentMode }, { 'text-black1': !shouldHeaderBgTransparentMode }]">{{
+            t(content.title)
+          }}</nuxt-link>
         </template>
+        <nuxt-link
+          :class="['text-sm font-bold ', { 'text-white': shouldHeaderBgTransparentMode }, { 'text-black1': !shouldHeaderBgTransparentMode }]"
+          :to="switchLocalePath(locale === 'en' ? 'ja' : 'en')"
+        >
+          <!-- 日本語 -->
+          {{ t("lang") }}
+        </nuxt-link>
       </div>
       <div class="md:hidden" @click="onClickMenu">
         <button class="h-8 w-9 block relative">
-          <div
-            :class="[
-              ' h-0.5 w-9 absolute top-1',
-              { 'bg-white': shouldHeaderBgTransparentMode },
-              { 'bg-black1': !shouldHeaderBgTransparentMode },
-            ]"
-          ></div>
-          <div
-            :class="[
-              ' h-0.5 w-9 absolute top-3',
-              { 'bg-white': shouldHeaderBgTransparentMode },
-              { 'bg-black1': !shouldHeaderBgTransparentMode },
-            ]"
-          ></div>
-          <p
-            :class="[
-              'text-xs absolute top-4 text-center',
-              { 'text-white': shouldHeaderBgTransparentMode },
-              { 'text-black1': !shouldHeaderBgTransparentMode },
-            ]"
-          >
-            MENU
-          </p>
+          <div :class="[' h-0.5 w-9 absolute top-1', { 'bg-white': shouldHeaderBgTransparentMode }, { 'bg-black1': !shouldHeaderBgTransparentMode }]"></div>
+          <div :class="[' h-0.5 w-9 absolute top-3', { 'bg-white': shouldHeaderBgTransparentMode }, { 'bg-black1': !shouldHeaderBgTransparentMode }]"></div>
+          <p :class="['text-xs absolute top-4 text-center', { 'text-white': shouldHeaderBgTransparentMode }, { 'text-black1': !shouldHeaderBgTransparentMode }]">MENU</p>
         </button>
       </div>
     </div>
@@ -72,13 +49,7 @@
       </div>
     </div>
     <!-- サイドバー -->
-    <div
-      :class="[
-        'w-full h-full bg-white fixed top-0 z-10 transition-all duration-500',
-        { 'left-0': isPanelOpen },
-        { 'left-full': !isPanelOpen },
-      ]"
-    >
+    <div :class="['w-full h-full bg-white fixed top-0 z-10 transition-all duration-500', { 'left-0': isPanelOpen }, { 'left-full': !isPanelOpen }]">
       <div class="pt-8 px-6 mb-16 flex justify-between">
         <img src="~@/assets/img/parts/fast-notion-black.svg" alt="" class="w-36" />
         <div class="md:hidden" @click="onClickMenu">
@@ -96,24 +67,25 @@
         </div>
       </template>
       <div class="w-full flex justify-center items-center px-10 mt-8">
-        <a href="https://apps.apple.com/jp/app/fast-notion/id1505194382"
-          ><img src="~@/assets/img/parts/download-ios.svg" alt=""
-        /></a>
-        <a href="https://play.google.com/store/apps/details?id=jp.fastNotion&hl=ja&gl=US"
-          ><img src="~@/assets/img/parts/download-android.svg" alt=""
-        /></a>
+        <a href="https://apps.apple.com/jp/app/fast-notion/id1505194382"><img src="~@/assets/img/parts/download-ios.svg" alt="" /></a>
+        <a href="https://play.google.com/store/apps/details?id=jp.fastNotion&hl=ja&gl=US"><img src="~@/assets/img/parts/download-android.svg" alt="" /></a>
       </div>
     </div>
   </header>
 </template>
+
 <script setup lang="ts">
 const router = useRouter();
 const route = useRoute();
+const switchLocalePath = useSwitchLocalePath();
+const { locale, t } = useI18n();
+
 const shouldHeaderBgTransparentMode = computed<boolean>(() => {
-  if (route.fullPath === "/" || route.fullPath === "") {
+  if (route.fullPath === "/" || route.fullPath === "/en" || route.fullPath === "") {
     return true;
   } else return false;
 });
+
 const isPanelOpen = ref<boolean>(false);
 const shouldShowHeader = ref<boolean>(false);
 const onClickMenu = () => {
@@ -134,7 +106,7 @@ onMounted(() => {
   registerHeaderAnimationEvent();
 });
 const headerList = [
-  { title: "使い方", path: "/manual" },
+  { title: "manual", path: "/manual" },
   { title: "リリースノート", path: "/releases" },
   { title: "よくある質問", path: "/faq" },
   { title: "お問い合わせ", path: "/contact" },
@@ -144,3 +116,5 @@ const jumpToPath = (path: string) => {
   router.push(path);
 };
 </script>
+
+<i18n src="./headerMessage.json"></i18n>
